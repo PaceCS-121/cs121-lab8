f7aeafb310b942c99b3f5828fa850fa4fa9fbcdf
Leanne <lkeeley@pace.edu>
2023-10-16 10:22:15 -0400

    add files. preload code

---
+++ analyze_employees.py
@@ -0,0 +1,18 @@
+def main():
+    employees = {'Alice': {'salary': 100000, 'years_of_service': 12, 'department': 'Sales'}, 
+        'Bob': {'salary': 45000, 'years_of_service': 2, 'department': 'Sales'},
+        'Carol': {'salary': 50000, 'years_of_service': 5, 'department': 'Marketing'},
+        'Bryan': {'salary': 85000, 'years_of_service': 8, 'department': 'Marketing'},
+        'Dan': {'salary': 70000, 'years_of_service': 8, 'department': 'IT'},
+        'Erin': {'salary': 65000, 'years_of_service': 8, 'department': 'Engineering'},
+        'Frank': {'salary': 99000, 'years_of_service': 10, 'department': 'Product Development'},
+        'Grace': {'salary': 550000, 'years_of_service': 3, 'department': 'Executive Office'},
+    }
+    
+    # write your code here
+
+    
+    return
+
+if __name__ == '__main__':
+    main()
